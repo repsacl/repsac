a66cd9d1c78e4aff407cff4852d01d6ce9419bfc
repsac <caslandberg@gmail.com>
2025-02-23 13:28:53 +0100

    navbar fix

--- LandbergBygg/src/Components/NavBar.jsx
+++ LandbergBygg/src/Components/NavBar.jsx
@@ -8,10 +8,8 @@ function NavBar() {
     <>
         <nav className="flex items-center justify-between w-full p-4 border-b bg-rgb(240, 240, 240)">
 
-            <div className="text-gray-500 text-sm">Bygg</div>
-
-            <Link to={"/"} className="absolute left-1/2 transform -translate-x-1/2">
-                <img src="/LB_Logo.svg" alt="Logo" className="md:h-15 h-11 cursor-pointer p-1 m-0 transition-all ease-in hover:p-1.5" />
+            <Link to={"/"} className="justify-start bg-gray-100 rounded-xl">
+                <img src="/LB_Logo.svg" alt="Logo" className="md:h-14 h-9 cursor-pointer m-0 transition-all ease-in hover:p-0.5" />
             </Link>
 
             <ul className="flex md:space-x-6 space-x-4 md:text-xl text-sm font-medium md:px-6 px-1">
